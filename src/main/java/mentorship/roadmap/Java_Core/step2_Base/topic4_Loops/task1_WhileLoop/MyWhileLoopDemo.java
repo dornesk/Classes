053964fd21030fe0ca3a22f075041cf88a5a304f
dornesk <dornesk@icloud.com>
2025-04-15 15:22:55 +0300

    Add class MyWhileLoopDemo

---
+++ src/main/java/mentorship/roadmap/Java_Core/step2_Base/topic4_Loops/task1_WhileLoop/MyWhileLoopDemo.java
@@ -0,0 +1,14 @@
+package mentorship.roadmap.Java_Core.step2_Base.topic4_Loops.task1_WhileLoop;
+
+public class MyWhileLoopDemo {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+
+        while (i <= 1000) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
