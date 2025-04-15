eaa488114bcdb3810c14139fa9b98fa01f7589ee
dornesk <dornesk@icloud.com>
2025-04-15 17:49:54 +0300

    Add class MySquareCalculator

---
+++ src/main/java/mentorship/roadmap/Java_Core/step2_Base/topic7_Methods/task5_SquareCalculator/MySquareCalculator.java
@@ -0,0 +1,12 @@
+package mentorship.roadmap.Java_Core.step2_Base.topic7_Methods.task5_SquareCalculator;
+
+public class MySquareCalculator {
+
+    public static void main(String[] args) {
+        System.out.println(square(5));
+    }
+
+    public static int square(int number) {
+        return number * number;
+    }
+}
