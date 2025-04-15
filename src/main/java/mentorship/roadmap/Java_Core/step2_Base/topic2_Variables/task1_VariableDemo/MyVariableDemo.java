f8df13c1a46da183121d4b2bf92d9b2e5ea681f8
dornesk <dornesk@icloud.com>
2025-04-15 10:21:06 +0300

    Add class MyVariableDemo

---
+++ src/main/java/mentorship/roadmap/Java_Core/step2_Base/topic2_Variables/task1_VariableDemo/MyVariableDemo.java
@@ -0,0 +1,12 @@
+package mentorship.roadmap.Java_Core.step2_Base.topic2_Variables.task1_VariableDemo;
+
+public class MyVariableDemo {
+    public static void main(String[] args) {
+        int n = 4;
+        double x = 5.4;
+        boolean isTrue = true;
+        System.out.println("Тип int: " + n);
+        System.out.println("Тип double: " + x);
+        System.out.println("Тип boolean: " + isTrue);
+    }
+}
