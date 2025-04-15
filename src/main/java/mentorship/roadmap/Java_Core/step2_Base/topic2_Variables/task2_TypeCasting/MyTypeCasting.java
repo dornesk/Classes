962eee562ceece890d64f68a5e73bb2a3fb7b90f
dornesk <dornesk@icloud.com>
2025-04-15 10:30:31 +0300

    Add class MyTypeCasting

---
+++ src/main/java/mentorship/roadmap/Java_Core/step2_Base/topic2_Variables/task2_TypeCasting/MyTypeCasting.java
@@ -0,0 +1,11 @@
+package mentorship.roadmap.Java_Core.step2_Base.topic2_Variables.task2_TypeCasting;
+
+public class MyTypeCasting {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        System.out.println("исходное значение: " + n);
+        System.out.println("результат преобразования: " + (double) n);
+    }
+}
