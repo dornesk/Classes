7f6c66b7c60b4238d7fc59d279b05c06a33d56e9
dornesk <dornesk@icloud.com>
2025-04-15 09:43:45 +0300

    Add class MyPrintNumbers

---
+++ src/main/java/mentorship/roadmap/Java_Core/step2_Base/topic1_Console/task3_PrintNumbers/MyPrintNumbers.java
@@ -0,0 +1,9 @@
+package mentorship.roadmap.Java_Core.step2_Base.topic1_Console.task3_PrintNumbers;
+
+public class MyPrintNumbers {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i+1);
+        }
+    }
+}
