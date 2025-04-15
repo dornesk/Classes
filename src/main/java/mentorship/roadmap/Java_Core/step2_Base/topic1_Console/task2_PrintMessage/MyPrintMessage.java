e93dee24a1a299aed1f9a4277f8f34b7f420332b
dornesk <dornesk@icloud.com>
2025-04-15 09:40:01 +0300

    Add class MyPrintMessage

---
+++ src/main/java/mentorship/roadmap/Java_Core/step2_Base/topic1_Console/task2_PrintMessage/MyPrintMessage.java
@@ -0,0 +1,8 @@
+package mentorship.roadmap.Java_Core.step2_Base.topic1_Console.task2_PrintMessage;
+
+public class MyPrintMessage {
+    public static void main(String[] args) {
+        String str = "значение";
+        System.out.println(str);
+    }
+}
