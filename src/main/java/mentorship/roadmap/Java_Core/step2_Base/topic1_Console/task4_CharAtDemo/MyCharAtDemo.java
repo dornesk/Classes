64aaa5869204527a415a4c0999d08f406648979b
dornesk <dornesk@icloud.com>
2025-04-15 09:51:16 +0300

    Add class MyCharAtDemo

---
+++ src/main/java/mentorship/roadmap/Java_Core/step2_Base/topic1_Console/task4_CharAtDemo/MyCharAtDemo.java
@@ -0,0 +1,10 @@
+package mentorship.roadmap.Java_Core.step2_Base.topic1_Console.task4_CharAtDemo;
+
+public class MyCharAtDemo {
+    public static void main(String[] args) {
+
+        int index = 4;
+        String str = "строка";
+        System.out.println(str.charAt(index));
+    }
+}
