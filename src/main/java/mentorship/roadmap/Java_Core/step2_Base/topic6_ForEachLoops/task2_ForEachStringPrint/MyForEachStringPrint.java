38bcf7f7acbfaf19825a78000dc03a0cc1d3a635
dornesk <dornesk@icloud.com>
2025-04-15 16:27:23 +0300

    Add class MyForEachStringPrint

---
+++ src/main/java/mentorship/roadmap/Java_Core/step2_Base/topic6_ForEachLoops/task2_ForEachStringPrint/MyForEachStringPrint.java
@@ -0,0 +1,13 @@
+package mentorship.roadmap.Java_Core.step2_Base.topic6_ForEachLoops.task2_ForEachStringPrint;
+
+public class MyForEachStringPrint {
+
+    public static void main(String[] args) {
+
+        String[] arr = {"qwe", "rty", "uio", "asd"};
+
+        for (String a : arr) {
+            System.out.println(a);
+        }
+    }
+}
