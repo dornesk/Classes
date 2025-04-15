36e0fb1acb1d77f27150d91a39f405a4a7cc21e4
dornesk <dornesk@icloud.com>
2025-04-15 15:07:19 +0300

    Add class MyPositiveCheckTernary

---
+++ src/main/java/mentorship/roadmap/Java_Core/step2_Base/topic3_Conditionals/task6_PositiveCheckTernary/MyPositiveCheckTernary.java
@@ -0,0 +1,14 @@
+package mentorship.roadmap.Java_Core.step2_Base.topic3_Conditionals.task6_PositiveCheckTernary;
+
+public class MyPositiveCheckTernary {
+
+    public static void main(String[] args) {
+
+        int n = 5;
+        String value;
+
+        value = n > 0 ? "Положительное" : "Нулевое или отрицательное";
+
+        System.out.println(value);
+    }
+}
