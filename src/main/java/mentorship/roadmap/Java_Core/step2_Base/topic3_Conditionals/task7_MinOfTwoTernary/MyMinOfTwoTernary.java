e9ec472160050c37715ad3faf94d6cc51f23c8ab
dornesk <dornesk@icloud.com>
2025-04-15 15:12:16 +0300

    Add class MyMinOfTwoTernary

---
+++ src/main/java/mentorship/roadmap/Java_Core/step2_Base/topic3_Conditionals/task7_MinOfTwoTernary/MyMinOfTwoTernary.java
@@ -0,0 +1,15 @@
+package mentorship.roadmap.Java_Core.step2_Base.topic3_Conditionals.task7_MinOfTwoTernary;
+
+public class MyMinOfTwoTernary {
+
+    public static void main(String[] args) {
+
+        int x = 6;
+        int y = -6;
+        int min;
+
+        min = x < y ? x : y;
+
+        System.out.println(min);
+    }
+}
