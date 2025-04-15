9bb66153316481601196db127e7ac728e553f577
dornesk <dornesk@icloud.com>
2025-04-15 16:17:04 +0300

    Add changes to class MyReverseArray

--- src/main/java/mentorship/roadmap/Java_Core/step2_Base/topic5_Arrays/task3_ReverseArray/MyReverseArray.java
+++ src/main/java/mentorship/roadmap/Java_Core/step2_Base/topic5_Arrays/task3_ReverseArray/MyReverseArray.java
@@ -7,7 +7,7 @@ public class MyReverseArray {
         int[] arr = {1, 2, 5, 6, 5, 25, 10};
 
         for (int i = arr.length-1; i >= 0; i--) {
-            System.out.print(arr[i] + ", ");
+            System.out.print(arr[i] + " ");
         }
     }
 }
