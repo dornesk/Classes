e388064037476a90692b63ec0348e9708cf4ca46
dornesk <dornesk@icloud.com>
2025-04-15 15:34:51 +0300

    Add class MyForLoopPrint

---
+++ src/main/java/mentorship/roadmap/Java_Core/step2_Base/topic4_Loops/task4_ForLoopPrint/MyForLoopPrint.java
@@ -0,0 +1,11 @@
+package mentorship.roadmap.Java_Core.step2_Base.topic4_Loops.task4_ForLoopPrint;
+
+public class MyForLoopPrint {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 1000; i++) {
+            System.out.println(i);
+        }
+    }
+}
