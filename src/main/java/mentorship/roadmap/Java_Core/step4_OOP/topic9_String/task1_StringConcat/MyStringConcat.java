f9bdabef6df4765026a077eaf0111c7992e751d3
dornesk <dornesk@icloud.com>
2025-04-18 12:58:10 +0300

    Add class MyStringConcat

---
+++ src/main/java/mentorship/roadmap/Java_Core/step4_OOP/topic9_String/task1_StringConcat/MyStringConcat.java
@@ -0,0 +1,12 @@
+package mentorship.roadmap.Java_Core.step4_OOP.topic9_String.task1_StringConcat;
+
+public class MyStringConcat {
+
+    public static void main(String[] args) {
+
+        String s1 = "111";
+        String s2 = "222";
+
+        System.out.println(s1 + s2);
+    }
+}
