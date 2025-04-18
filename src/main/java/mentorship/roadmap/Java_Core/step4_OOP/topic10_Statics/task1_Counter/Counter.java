7b829d835e399ef7117db1987c247574c3d7bf87
dornesk <dornesk@icloud.com>
2025-04-18 13:15:09 +0300

    Add classes Counter, CounterDemo

---
+++ src/main/java/mentorship/roadmap/Java_Core/step4_OOP/topic10_Statics/task1_Counter/Counter.java
@@ -0,0 +1,14 @@
+package mentorship.roadmap.Java_Core.step4_OOP.topic10_Statics.task1_Counter;
+
+public class Counter {
+
+    private static int count;
+
+    public Counter() {
+        count++;
+    }
+
+    public static void getCount() {
+        System.out.println("Значение счетчика: " + count);
+    }
+}
---
+++ src/main/java/mentorship/roadmap/Java_Core/step4_OOP/topic10_Statics/task1_Counter/CounterDemo.java
@@ -0,0 +1,14 @@
+package mentorship.roadmap.Java_Core.step4_OOP.topic10_Statics.task1_Counter;
+
+public class CounterDemo {
+
+    public static void main(String[] args) {
+
+        new Counter();
+        new Counter();
+        new Counter();
+        new Counter();
+
+        Counter.getCount();
+    }
+}
