f2c953b519c7c49ec6890d0bf09c9ca8c8c41418
dornesk <dornesk@icloud.com>
2025-04-18 11:29:18 +0300

    Add classes Computer, ComputerDemo

---
+++ src/main/java/mentorship/roadmap/Java_Core/step4_OOP/topic8_Computer/ComputerDemo.java
@@ -0,0 +1,11 @@
+package mentorship.roadmap.Java_Core.step4_OOP.topic8_Computer;
+
+public class ComputerDemo {
+
+    public static void main(String[] args) {
+
+        Computer computer = new Computer("qwerty", "1234", 1024);
+
+        computer.displaySpecs();
+    }
+}
