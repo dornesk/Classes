dace0ffb5ea3ade428d79e0d54dd71c2ec121cad
dornesk <dornesk@icloud.com>
2025-04-18 13:03:52 +0300

    Add class MyStringLength

---
+++ src/main/java/mentorship/roadmap/Java_Core/step4_OOP/topic9_String/task4_StringLength/MyStringLength.java
@@ -0,0 +1,11 @@
+package mentorship.roadmap.Java_Core.step4_OOP.topic9_String.task4_StringLength;
+
+public class MyStringLength {
+
+    public static void main(String[] args) {
+
+        String s1 = "1111111111";
+
+        System.out.println("Длина строки : " + s1.length());
+    }
+}
