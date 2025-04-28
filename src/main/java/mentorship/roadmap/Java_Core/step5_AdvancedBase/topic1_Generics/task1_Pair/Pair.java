2e97a9b6a578bce0e94991fa56befd0793c4c20f
dornesk <dornesk@icloud.com>
2025-04-28 08:29:11 +0300

    Add class PairDemo, add changes to class Pair
    Signed-off-by: dornesk <dornesk@icloud.com>

--- src/main/java/mentorship/roadmap/Java_Core/step5_AdvancedBase/topic1_Generics/task1_Pair/Pair.java
+++ src/main/java/mentorship/roadmap/Java_Core/step5_AdvancedBase/topic1_Generics/task1_Pair/Pair.java
@@ -1,4 +1,34 @@
 package mentorship.roadmap.Java_Core.step5_AdvancedBase.topic1_Generics.task1_Pair;
 
-public class Pair {
+public class Pair <T, U> {
+    private T first;
+    private U second;
+
+    public Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public U getSecond() {
+        return second;
+    }
+
+    public void setSecond(U second) {
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair: " + this.first + ", " + this.second;
+    }
+
+
 }
---
+++ src/main/java/mentorship/roadmap/Java_Core/step5_AdvancedBase/topic1_Generics/task1_Pair/PairDemo.java
@@ -0,0 +1,9 @@
+package mentorship.roadmap.Java_Core.step5_AdvancedBase.topic1_Generics.task1_Pair;
+
+public class PairDemo {
+
+    public static void main(String[] args) {
+        Pair <String, Integer> pair = new Pair<>("str", 3);
+        System.out.println(pair);
+    }
+}
