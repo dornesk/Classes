f36ca645597b4eb930b201eabe8dd47de9ab155c
dornesk <dornesk@icloud.com>
2025-05-06 13:24:10 +0300

    Add classes Day, DayDemo

---
+++ src/main/java/mentorship/roadmap/Java_Core/step5_AdvancedBase/topic3_Enums/task1_Day/DayDemo.java
@@ -0,0 +1,10 @@
+package mentorship.roadmap.Java_Core.step5_AdvancedBase.topic3_Enums.task1_Day;
+
+public class DayDemo {
+
+    public static void main(String[] args) {
+        for (Day d : Day.values()) {
+            System.out.println(d);
+        }
+    }
+}
