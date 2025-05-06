afb47e7df0443c52d5f299e6eec6e2c001ff8273
dornesk <dornesk@icloud.com>
2025-05-06 14:20:25 +0300

    Add classes Season, SeasonDemo

---
+++ src/main/java/mentorship/roadmap/Java_Core/step5_AdvancedBase/topic3_Enums/task4_Season/Season.java
@@ -0,0 +1,16 @@
+package mentorship.roadmap.Java_Core.step5_AdvancedBase.topic3_Enums.task4_Season;
+
+public enum Season {
+    WINTER(-10), SPRING(10), SUMMER(20), FALL(15);
+
+    private final double averageTemp;
+
+    Season(double averageTemp) {
+        this.averageTemp = averageTemp;
+    }
+
+    @Override
+    public String toString() {
+        return "Season: " + name() + ", Avg Temp: " + averageTemp + "°C";
+    }
+}
