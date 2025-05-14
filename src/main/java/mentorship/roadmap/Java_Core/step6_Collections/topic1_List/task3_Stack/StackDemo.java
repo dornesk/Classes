4cb1de215fa349627981cda9552911507804b0ce
dornesk <dornesk@icloud.com>
2025-05-14 14:09:19 +0300

    Add class StackDemo
    Signed-off-by: dornesk <dornesk@icloud.com>

---
+++ src/main/java/mentorship/roadmap/Java_Core/step6_Collections/topic1_List/task3_Stack/StackDemo.java
@@ -0,0 +1,21 @@
+package mentorship.roadmap.Java_Core.step6_Collections.topic1_List.task3_Stack;
+
+import java.util.Stack;
+
+public class StackDemo {
+
+    public static void main(String[] args) {
+
+        Stack<String> stack = new Stack<>();
+        stack.push("First");
+        stack.push("Second");
+        stack.push("Third");
+
+        System.out.println(stack.peek());
+        System.out.println();
+
+        while (stack.iterator().hasNext()) {
+            System.out.println(stack.pop());
+        }
+    }
+}
