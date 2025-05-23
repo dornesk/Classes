4f9665f1717f08f2dbd77552f771a898d16aaa42
dornesk <dornesk@icloud.com>
2025-05-23 12:17:23 +0300

    Add class DeadlockDemo
    Signed-off-by: dornesk <dornesk@icloud.com>

--- src/main/java/mentorship/roadmap/Java_Core/step8_Multithereading/topic8_Deadlock/DeadlockDemo.java
+++ src/main/java/mentorship/roadmap/Java_Core/step8_Multithereading/topic8_Deadlock/DeadlockDemo.java
@@ -1,18 +1,21 @@
 package mentorship.roadmap.Java_Core.step8_Multithereading.topic8_Deadlock;
 
 public class DeadlockDemo {
+    private static Object lock1 = new Object(); // объекты для синхронизации
+    private static Object lock2 = new Object();
+
     public static void main(String[] args) {
-        Object lock1 = new Object(); // объекты для синхронизации
-        Object lock2 = new Object();
 
         Thread t1 = new Thread(() -> {
             synchronized (lock1) {
                 System.out.println("Захват потоком №1 замка lock1");
                 try {
-                    Thread.sleep(500);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    Thread.currentThread().interrupt();
                 }
+
+                System.out.println("Ожидание захвата потоком №1 замка lock2");
                 synchronized (lock2) {
                     System.out.println("Захват потоком №1 замка lock2");
                 }
@@ -23,10 +26,12 @@ public class DeadlockDemo {
             synchronized (lock2) {
                 System.out.println("Захват потоком №2 замка lock2");
                 try {
-                    Thread.sleep(500);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    Thread.currentThread().interrupt();
                 }
+
+                System.out.println("Ожидание захвата потоком №2 замка lock1");
                 synchronized (lock1) {
                     System.out.println("Захват потоком №2 замка lock1");
                 }
