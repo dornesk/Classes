7669e217aa330a92f70bbcb2e6992a95fee48e4a
dornesk <dornesk@icloud.com>
2025-05-26 11:51:53 +0300

    Added test MaxFinderTest, improvements to class MaxFinder
    Signed-off-by: dornesk <dornesk@icloud.com>

--- src/main/java/mentorship/roadmap/Java_Core/step9_Test/topic1_Junit/task2_MaxFinder/MaxFinder.java
+++ src/main/java/mentorship/roadmap/Java_Core/step9_Test/topic1_Junit/task2_MaxFinder/MaxFinder.java
@@ -3,7 +3,7 @@ package mentorship.roadmap.Java_Core.step9_Test.topic1_Junit.task2_MaxFinder;
 public class MaxFinder {
     public static int findMax(int[] numbers) {
         if (numbers == null || numbers.length == 0) {
-            throw new IllegalArgumentException("The array is empty.")
+            throw new IllegalArgumentException("The array is empty.");
         }
 
         int max = numbers[0];
