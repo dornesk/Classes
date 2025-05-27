4b6623a2d3c89cfb8d218d57424260fdd2ce563a
dornesk <dornesk@icloud.com>
2025-05-27 13:47:11 +0300

    Added test AuthServiceTest
    Signed-off-by: dornesk <dornesk@icloud.com>

---
+++ src/main/java/mentorship/roadmap/Java_Core/step9_Test/topic2_Mockito/task2_AuthService/AuthServiceTest.java
@@ -0,0 +1,55 @@
+package mentorship.roadmap.Java_Core.step9_Test.topic2_Mockito.task2_AuthService;
+
+import mentorship.roadmap.Java_Core.step9_Test.topic2_Mockito.task2_AuthService.forTest.AuthService;
+import mentorship.roadmap.Java_Core.step9_Test.topic2_Mockito.task2_AuthService.forTest.User;
+import mentorship.roadmap.Java_Core.step9_Test.topic2_Mockito.task2_AuthService.forTest.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
+public class AuthServiceTest {
+    private UserRepository userRepository;
+    private AuthService authService;
+
+    @BeforeEach
+    void setUp() {
+        userRepository = mock(UserRepository.class);
+        authService = new AuthService(userRepository);
+    }
+
+    @Test
+    void authenticateUserSuccess() {
+        User user = new User("user1", "password1");
+        when(userRepository.findUserByUsername(user.getUsername())).thenReturn(user);
+
+        boolean result = authService.authenticateUser("user1", "password1");
+        assertTrue(result);
+
+        verify(userRepository).findUserByUsername(user.getUsername());
+    }
+
+    @Test
+    void authenticateUserFailurePasswordIsWrong() {
+        User user = new User("user2", "password2");
+        when(userRepository.findUserByUsername(user.getUsername())).thenReturn(user);
+
+        boolean result = authService.authenticateUser("user2", "wrongpassword");
+        assertFalse(result);
+
+        verify(userRepository).findUserByUsername(user.getUsername());
+    }
+
+    @Test
+    void authenticateUserFailureUserNotFound() {
+        User user = new User("user3", "password3");
+        when(userRepository.findUserByUsername("notfound")).thenReturn(null);
+
+        boolean result = authService.authenticateUser("notfound", "password3");
+        assertFalse(result);
+
+        verify(userRepository).findUserByUsername("notfound");
+    }
+}
