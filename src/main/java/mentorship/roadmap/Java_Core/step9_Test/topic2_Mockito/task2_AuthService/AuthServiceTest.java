6a66b975717c35165634e9b464272f8f96f48f1f
dornesk <dornesk@icloud.com>
2025-05-27 13:52:06 +0300

    Added changes to AuthServiceTest
    Signed-off-by: dornesk <dornesk@icloud.com>

--- src/main/java/mentorship/roadmap/Java_Core/step9_Test/topic2_Mockito/task2_AuthService/AuthServiceTest.java
+++ src/main/java/mentorship/roadmap/Java_Core/step9_Test/topic2_Mockito/task2_AuthService/AuthServiceTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
-public class AuthServiceTest {
+class AuthServiceTest {
     private UserRepository userRepository;
     private AuthService authService;
 
